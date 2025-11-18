7a0a3e7308081a7e800d00513756c3ebb5b0fda0
santoescu <santoescu@gmail.com>
2025-11-18 10:57:36 -0500

    [FIX] dockerfile

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,13 +2,14 @@
     <flux:main>
         {{ $slot }}
     </flux:main>
+
     @livewireScripts
+    @fluxScripts
+
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
     <!-- DataTables -->
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-
     <!-- Preline -->
     <script src="https://unpkg.com/preline/dist/preline.js"></script>
 
