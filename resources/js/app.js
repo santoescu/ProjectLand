d56cc5af1e8665192b820dc1a24a0b93ae9d5e96
santoescu <santoescu@gmail.com>
2025-11-19 10:26:30 -0500

    [FIX] diseños

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import "preline";
 import '@preline/select'
 
-//import './vendor/flux.min.js';
+import './vendor/flux.min.js';
 import $ from 'jquery';
 import '@preline/copy-markup';
 import 'datatables.net-dt';
