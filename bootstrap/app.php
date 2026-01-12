a4dbc7fe7c8665549bd5e4713d4fe5106750973d
santoescu <santoescu@gmail.com>
2026-01-11 23:08:22 -0500

    [NEW] radio gruop appearance

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\LoadAppearanceFromUser;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
+        $middleware->web(LoadAppearanceFromUser::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
